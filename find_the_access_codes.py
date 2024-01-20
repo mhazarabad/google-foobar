368224cca5ef81dad1f91ab96237b84095c9c0ee
M Hadi Azarabad <mhazarabad@gmail.com>
2024-01-20 01:33:07 +0100

    all tests passed, last test was run time check :)

--- find_the_access_codes.py
+++ find_the_access_codes.py
@@ -1,24 +1,23 @@
 def solution(l):
-    if len(l)>2000:return 0
     if len(l)<3:return 0
-    l= [int(i) for i in l if str(i).isdigit() and 1<=int(i)<=999999]
-    codes = []
+    number_of_codes = 0
     for idx,value in enumerate(l):
         left_side_possible_codes_to_pair = l[:idx]
+        left_side_possible_codes_to_pair = sum(left_side_possible_codes_to_pair.count(code) for code in set(left_side_possible_codes_to_pair) if value%code==0)
         right_side_possible_codes_to_pair = l[idx+1:]
-        left_side_pair_codes = [code for code in left_side_possible_codes_to_pair if value % code == 0]
-        right_side_pair_codes = [code for code in right_side_possible_codes_to_pair if code % value == 0]
-        for left_code in left_side_pair_codes:
-            for right_code in right_side_pair_codes:
-                codes.append((left_code,value,right_code))
-    return len(codes)
+        right_side_possible_codes_to_pair = sum(right_side_possible_codes_to_pair.count(code) for code in set(right_side_possible_codes_to_pair) if code%value==0)
+        number_of_codes += (left_side_possible_codes_to_pair)*(right_side_possible_codes_to_pair)
+    return number_of_codes
 
 
 
 
 # print(solution([1,2,3,4,5,6]))#3
+# print(solution([1,2]))#0
+# print(solution([1]))#0
+# print(solution([4,2,2,6,5,6]))#4
 # print(solution([1,1,1]))#1
 # print(solution([1,1,1,1]))#4
 # print(solution([1,2,3]))#0
-# assert solution([1]*2000) == 0
-
+# print(solution([1,2,3,3]))#1
+print(solution([1]*2000))
