a7be6cd287108c308b57ab8d2f7f31c0d40b062b
M Hadi Azarabad <mhazarabad@gmail.com>
2024-01-26 12:11:13 +0100

    6,7,9 not passed - using Fraction built in

--- dooms_day_fuel.py
+++ dooms_day_fuel.py
@@ -3,69 +3,8 @@ def solution(m):
     min_height = len(m)
     sqaure_size = min(min_width, min_height)
     m = [row[:sqaure_size] for row in m[:sqaure_size]]
-    class Fraction:
-        def __init__(self,nominator,denominator):
-            self.nominator = nominator
-            self.denominator = denominator
-        
-        @staticmethod
-        def gcd(a, b, rtol = 1e-05, atol = 1e-08):
-            t = min(abs(a), abs(b))
-            while abs(b) > rtol * t + atol:
-                a, b = b, a % b
-            return a
-
-        def simplify(self):
-        
-            return Fraction(self.nominator//self.gcd(self.nominator, self.denominator), self.denominator//self.gcd(self.nominator, self.denominator))
-
-        def __add__(self, other):
-            if isinstance(other, int):
-                return Fraction(self.nominator + other * self.denominator, self.denominator).simplify()
-            elif isinstance(other, Fraction):
-                return Fraction(self.nominator * other.denominator + other.nominator * self.denominator, self.denominator * other.denominator).simplify()
-            else:
-                raise NotImplementedError
-        def __sub__(self, other):
-            if isinstance(other, int):
-                return Fraction(self.nominator - other * self.denominator, self.denominator).simplify()
-            elif isinstance(other, Fraction):
-                return Fraction(self.nominator * other.denominator - other.nominator * self.denominator, self.denominator * other.denominator).simplify()
-            else:
-                raise NotImplementedError
-        def __mul__(self, other):
-            if isinstance(other, int):
-                return Fraction(self.nominator * other, self.denominator).simplify()
-            elif isinstance(other, Fraction):
-                return Fraction(self.nominator * other.nominator, self.denominator * other.denominator).simplify()
-            else:
-                raise NotImplementedError
-        def __truediv__(self, other):
-            if isinstance(other, int):
-                return Fraction(self.nominator, self.denominator * other).simplify()
-            elif isinstance(other, Fraction):
-                return Fraction(self.nominator * other.denominator, self.denominator * other.nominator).simplify()
-            else:
-                raise NotImplementedError
-        def __floordiv__(self, other):
-            if isinstance(other, int):
-                return Fraction(self.nominator, self.denominator * other).simplify()
-            elif isinstance(other, Fraction):
-                return Fraction(self.nominator * other.denominator, self.denominator * other.nominator).simplify()
-            else:
-                raise NotImplementedError
-        def __mod__(self, other):
-            if isinstance(other, int):
-                return Fraction(self.nominator, self.denominator * other).simplify()
-            elif isinstance(other, Fraction):
-                return Fraction(self.nominator * other.denominator, self.denominator * other.nominator).simplify()
-            else:
-                raise NotImplementedError
-        def __div__(self, other):
-            return Fraction(self.nominator * other.denominator, self.denominator * other.nominator).simplify()
-        def __eq__(self, other):
-            self.nominator * other.denominator == other.nominator * self.denominator
-
+    from fractions import Fraction
+    
     for row_idx, row in enumerate(m):
         for col_idx, col in enumerate(row):
             if col < 0:
@@ -123,12 +62,18 @@ def solution(m):
         return C
     FR = multiply(F,R)[0]
     
+    def gcd(a, b, rtol = 1e-05, atol = 1e-08):
+        t = min(abs(a), abs(b))
+        while abs(b) > rtol * t + atol:
+            a, b = b, a % b
+        return a
+
     def lcm(a, b):
-        return abs(a*b) // Fraction.gcd(a, b)
+        return abs(a*b) // gcd(a, b)
     denominator = 1
     for probability in FR:
         denominator = lcm(denominator, probability.denominator)
-    return [probability.nominator * (denominator//probability.denominator) for probability in FR] + [denominator]
+    return [probability.numerator * (denominator//probability.denominator) for probability in FR] + [denominator]
 
 test_1=[  
     [0,1,0,0,0,1],  # s0, the initial state, goes to s1 and s5 with equal probability  
